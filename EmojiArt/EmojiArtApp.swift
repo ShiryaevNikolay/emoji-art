aca072b5b8d9db286ac07b14b8a123723ab32114
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-09-12 08:52:59 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by shiryaev on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
